bba2c1badfdf0f80de9445baa4d8009f3f919fa1
Tobias Lundin <tobias.lundin@gmail.com>
2021-11-25 21:46:07 +0100

    add proxy mapper

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
 import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 
+const pathMapper = new Map<string, string>();
+pathMapper.set('pages', 'https://contentlayout.rikstv.no/1');
+pathMapper.set('menus', 'https://contentlayout.rikstv.no/1');
+pathMapper.set('assets', 'https://contentsearch.rikstv.no/1');
+pathMapper.set('client', 'https://api.rikstv.no/client/2');
+
 function handler(_req: Request): Response {
-  const {
-    body,
-    headers,
-    method,
-    url,
-  } = _req;
-  const res = JSON.stringify({body,headers,method,url});
-  return new Response(res, {
+  const { headers, method, url } = _req;
+
+  const { pathname, search } = new URL(url);
+  const upstream = pathMapper.get(pathname.split('/')[0]);
+
+  if (!upstream) {
+    return new Response(JSON.stringify({ message: `found no mapping for path: ${pathname}${search}` }), {
+      headers: { "content-type": "application/json; charset=utf-8" },
+      status: 404
+    });
+  }
+
+  return new Response(
+    JSON.stringify({ req: {headers,method,url }, res: { upstream, pathAndQuery: pathname + search } }), {
     headers: { "content-type": "application/json; charset=utf-8" },
   });
 }
