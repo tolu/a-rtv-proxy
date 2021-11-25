5133df80cded741457be196af1a842c14e192d9e
Tobias Lundin <tobias.lundin@gmail.com>
2021-11-25 23:37:08 +0100

    move properties

--- mappers/assets.ts
+++ mappers/assets.ts
@@ -17,21 +17,24 @@ export const mapAsset = (assetList: Asset[]) => {
     const { id, name, imagePackUri, duration, description, imdbRating } = asset;
     const images = [300, 600].map(getImageSizeMapper(imagePackUri));
     const mappedAsset: MappedAsset = {
-      title: name, images, duration, description, imdbRating: parseFloat(imdbRating.toFixed(1)),
-      link: `https://www.strim.no/watch/vod/${id}`
+      title: name,
+      description,
+      duration,
+      imdbRating: parseFloat(imdbRating.toFixed(1)),
+      link: `https://www.strim.no/watch/vod/${id}`,
     };
 
     // override and add properties for series
     if (isEpisodeAsset(asset)) {
       mappedAsset.link = `https://www.strim.no/watch/seriesoverview/${asset.seriesId}`;
       mappedAsset.series = {
-        images: [300, 600].map(getImageSizeMapper(imagePackUri, 'series-main')),
         title: asset.seriesName,
         availableSeasons: asset.availableSeasons,
+        images: [300, 600].map(getImageSizeMapper(imagePackUri, 'series-main')),
       }
     }
 
-    return {...mappedAsset, __originalAsset: asset};
+    return {...mappedAsset, images, __originalAsset: asset};
   });
 }
 
@@ -44,7 +47,7 @@ interface MappedAsset {
   description: string;
   imdbRating: number;
   link: string;
-  images: Array<{width: number, url: string}>;
+  images?: Array<{width: number, url: string}>;
   series?: {
     title: string;
     availableSeasons: number;
