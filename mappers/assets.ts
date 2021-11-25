9276534516c336509e4cc9489c7357b486c74647
Tobias Lundin <tobias.lundin@gmail.com>
2021-11-25 22:56:07 +0100

    start mapping assets

--- mappers/assets.ts
+++ mappers/assets.ts
@@ -1,10 +1,29 @@
 export const mapAsset = (assetList: Asset[]) => {
-  return assetList.map(a => {
-    a.__mapped = true;
-    return a;
+  return assetList.map((asset) => {
+    const { id, name, imagePackUri, duration, description, imdbRating } = asset;
+    return {
+      id, name, imagePackUri, duration, description, imdbRating,
+      __originalAsset: asset
+    };
   });
 }
 
 interface Asset {
-  __mapped: boolean;
+  id: string;
+  name: string;
+  imagePackUri: string;
+  duration: number;
+  description: string;
+  imdbRating: number;
+  // potential series properties
+  seriesName?: string;
+  seasonDescription?: string;
+  season?: number;
+  episode?: number;
+  availableSeasons?: number;
+  seriesId?: string;
+
+  // unused
+  episodeCount?: number;
+  availableEpisodes?: number;
 }
