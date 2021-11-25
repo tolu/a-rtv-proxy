10cfdfc56db0449104c69bdc104e85b3f8b906b8
Tobias Lundin <tobias.lundin@gmail.com>
2021-11-25 21:50:57 +0100

    fix pathsegment

--- index.ts
+++ index.ts
@@ -10,10 +10,11 @@ function handler(_req: Request): Response {
   const { headers, method, url } = _req;
 
   const { pathname, search } = new URL(url);
-  const upstream = pathMapper.get(pathname.split('/')[0]);
+  const firstPathSegment = pathname.split('/').filter(Boolean)[0];
+  const upstream = pathMapper.get(firstPathSegment);
 
   if (!upstream) {
-    return new Response(JSON.stringify({ message: `found no mapping for path: ${pathname}${search}` }), {
+    return new Response(JSON.stringify({ message: `found no mapping for path: ${firstPathSegment}` }), {
       headers: { "content-type": "application/json; charset=utf-8" },
       status: 404
     });
