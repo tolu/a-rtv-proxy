2691d7f50176c78c5198744c2e44b1b4f7afa9ba
Tobias Lundin <tobias.lundin@gmail.com>
2021-11-28 22:17:42 +0100

    add more info to item renderer

--- middleware/htmlMiddleware.tsx
+++ middleware/htmlMiddleware.tsx
@@ -52,7 +52,8 @@ async function pageListRenderer(pageList: PageList) {
 }
 
 async function pageRenderer({title, swimlanes }: Page) {
-  const assetLists = await Promise.all(swimlanes.slice(1, 5).map(async (list) => {
+  const supportedLanes = swimlanes.filter(s => s.type !== 'Menu').slice(1, 5);
+  const assetLists = await Promise.all(supportedLanes.map(async (list) => {
     const items = await (await fetch(list.link)).json();
     return { ...list, items };
   }));
@@ -63,7 +64,7 @@ async function pageRenderer({title, swimlanes }: Page) {
     ${ assetLists.map(s => /*html*/`
       <div>
         <h2>${s.name} (${s.type} : ${s.style})</h2>
-        <div style="overflow-x: auto; display: flex">
+        <div style="overflow-x: auto; display: flex" class="swimlane">
         ${ s.items.map(itemRenderer).join('') }
         </div>
       </div>
@@ -73,11 +74,32 @@ async function pageRenderer({title, swimlanes }: Page) {
 }
 
 function itemRenderer(item: MappedAsset) {
+  const startTime = item.startTimeEpoch ? new Date(item.startTimeEpoch) : null;
+  const endTime = item.startTimeEpoch && item.durationInSeconds ? new Date(item.startTimeEpoch + item.durationInSeconds * 1000) : null;
   return /*html*/`
-  <div style="margin-right: 1rem">
-    <picture width="300">
-      <img src="${item.image[0].url}" />
-    </picture>
+  <div style="margin-right: 1rem; position: relative">
+    <div style="position:relative" class="image-wrapper">
+      <picture>
+        <img width="300" loading="lazy" src="${item.image[0].url}" />
+      </picture>
+      <img src="${item.providerLogoUrl}" width="40" style="border-radius: 50vw; position: absolute; right: 10px; top: 10px;" />
+      ${renderProgressBar(startTime, endTime)}
+    </div>
+    <h3>${item.title}</h3>
+    <p class="hover">${item.subtitle}</p>
   </div>
   `.trim();
+}
+
+function renderProgressBar(start: Date | null, end: Date | null) {
+  if (!start || !end) return '';
+  const now = Date.now();
+  const startTime = start.getTime();
+  const endTime = end.getTime();
+  if (now > endTime) return '';
+  if (now < startTime) return '';
+  const progress = Math.round((now - startTime) / (endTime - startTime) * 100);
+  return /*html*/`<div style="border-radius: 50vw; overflow: hidden; background: rgba(100 100 100 / 50%); position: relative; bottom: 13px; width: 95%; margin: auto;">
+    <div style="height: 5px; width: ${progress}%; background: rgba(255 255 0 / 75%);"></div>
+  </div>`;
 }
--- utils/html.ts
+++ utils/html.ts
@@ -3,6 +3,9 @@ export const appShell = (bodyHtml: string) => {
   <html>
     <head>
       <style>
+        :root {
+          --body-width: 80ch;
+        }
         html {
           font: normal 400 16px/1.5 system-ui, sans-serif;
           color: #e8e6e3;
@@ -10,7 +13,7 @@ export const appShell = (bodyHtml: string) => {
         }
         body {
           margin: auto;
-          max-width: 70ch;
+          max-width: var(--body-width);
           padding-block: 2rem;
         }
         a {
@@ -18,6 +21,25 @@ export const appShell = (bodyHtml: string) => {
           transition: transform 400ms;
         }
         a:hover { transform: scale(1.35) }
+        h2 { margin-bottom: 0 }
+        .swimlane {
+          padding-top: 1rem;
+          margin-left: calc(-97vw / 2 + var(--body-width) / 2);
+          margin-right: calc(-97vw / 2 + var(--body-width) / 2);
+        }
+        .swimlane h3 {
+          margin: .35rem 0 .25rem;
+          font-size: 1rem;
+        }
+        .swimlane p {
+          color: #999;
+          font-size: .875rem;
+          margin-block: 0;
+        }
+        .hover { opacity: 0; transition: opacity 300ms; }
+        .swimlane > div:hover .hover { opacity: 1 }
+        .swimlane [loading=lazy] { transition: transform 300ms; transform: scale(1); border-radius: 5px; }
+        .swimlane > div:hover [loading=lazy] { transform: scale(1.05) }
       </style>
     </head>
     <body>
