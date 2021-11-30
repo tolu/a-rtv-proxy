476c5a1d511f61e6e41afe364dec7fc876ee2838
Tobias Lundin <tobias.lundin@gmail.com>
2021-11-30 01:01:18 +0100

    space

--- middleware/htmlMiddleware.tsx
+++ middleware/htmlMiddleware.tsx
@@ -27,13 +27,16 @@ export const htmlMiddlewareHandler = async (_req: Request) => {
   }
   
   console.log('got data', {data});
+  
   try {
+
     const renderer = templateMap.get(firstPathSegment) ?? defaultRenderer;
     const html = appShell(await renderer(data));
   
     return new Response(html, {
       headers: { 'content-type': 'text/html; charset=utf-8' },
     });
+
   } catch (err) {
     return new Response(JSON.stringify(err, null, 2));
   }
